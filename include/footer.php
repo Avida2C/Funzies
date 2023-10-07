66201ef395d3291e8af0b094457b587bcc5c167e
Nadine <n4dinevid4l@hotmail.com>
2023-10-07 21:19:07 +0200

    -functions.php has been created and linked in the HTML
    -HTML code has been seperated in the php files found in the include folder and have been linked with the index.php using the require_once statement.

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+<footer>
+
+</footer>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!-- Javascript -->
+<script src="js/javascript.js"></script>
+<script src="js/jquery-3.7.1.min.js"></script>
+
+<!-- Scripts --> 
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,44 +1,21 @@
 <?php 
-
+// functions.php will contain any functionalities which may be required on more than one page. 
+require 'functions.php';
 ?>
 
 <?php 
-
+require_once 'header.php';
+// Start of Body //
+require_once 'navbar.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- StyleSheet -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-</head>
-<body>
-    <nav>
-
-    </nav>
 
     
 
-<footer>
-
-</footer>
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<!-- Javascript -->
-<script src="js/javascript.js"></script>
-<script src="js/jquery-3.7.1.min.js"></script>
-
-<!-- Scripts --> 
+    
 
 
-</body>
-</html>
 
 <?php  
-
+require_once 'footer.php';
+// End of Body //
 ?>
