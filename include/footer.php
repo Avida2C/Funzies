4552ba137fdc440130f7e87f4a155db2f4593c7e
Nadine <n4dinevid4l@hotmail.com>
2023-10-07 23:35:19 +0200

    created the footer and personalized it as necessary, using bootstrap and css. the logo has also been created.

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,82 @@
-<footer>
+<footer class="text-center text-lg-start bg-light text-muted">
+
+    <div class="container col-md-10 w-100 pt-4">
+        <div class="row">
+            <div class="col-3">
+                <img class="w-100 px-5" src="img/logo.png" alt="logo">
+            </div>
+            <div class="col-2">
+                <ul class="list-unstyled">
+                    <li><b>Account</b></li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Sign Up</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Login</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Shopping Cart</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-3">
+                <ul class="list-unstyled">
+                    <li><b>Funzies Collection</b></li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Terms & Conditions</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Careers</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul class="list-unstyled">
+                    <li><b>Help & Support</b></li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Returns & Warranty</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-muted">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul class="list-unstyled text-center">
+                    <li><b>Find Us</b></li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-dark">
+                            <ion-icon size="large" name="logo-facebook"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-dark">
+                            <ion-icon size="large" name="logo-instagram"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-decoration-none text-dark">
+                            <ion-icon size="large" name="logo-twitter"></ion-icon>
+                            </ion-icon>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="row bg-light border-top pt-3">
+            <div class="col ms-5">
+                <p> Copyright &copy; <?php echo date("Y") ?> Funzies Collection. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
 
 </footer>
 <!-- Bootstrap JS -->
@@ -7,7 +85,10 @@
 </script>
 <!-- Javascript -->
 <script src="js/javascript.js"></script>
+<!-- JQuery -->
 <script src="js/jquery-3.7.1.min.js"></script>
+<!-- Ionicons -->
+<script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></script>
 
 <!-- Scripts -->
 
