dc9048fdc9592fd0353fa36ea4881ce08ad16e15
Nadine <n4dinevid4l@hotmail.com>
2023-10-08 23:49:41 +0200

    changed search button in navbar to search icon

--- include/navbar.php
+++ include/navbar.php
@@ -1,16 +1,16 @@
 <nav class="text-center text-lg-start bg-light text-muted">
-    <div class="container col-lg-8 col-md-10 col-sm-10 align-items-center py-4">
+    <div class="container col-lg-8 col-md-10 col-sm-10 align-items-center py-2">
         <div class="row">
             <div id="navAccount" class="col-lg-4 col-sm-4 text-start ">
                 <img class="navLogo" src="img/logo.png" alt="">
             </div>
-            <div id="navSearchbar" class="col-lg-5 col-sm-4 py-2 ">
+            <div id="navSearchbar" class="col-lg-5 col-sm-4 py-1">
                 <form method="get" class="d-flex">
                     <input class="rounded border w-100 p-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-secondary ms-1 px-2" type="submit">Search</button>
+                    <button class="rounded ms-1 border bg-light" type="submit"><ion-icon size="large" name="search-outline"></ion-icon></button>
                 </form>
             </div>
-            <div id="navAccount" class="col-lg-3 col-sm-4 text-end py-3">
+            <div id="navAccount" class="col-lg-3 col-sm-4 text-end py-2">
                 <a href="#" class="text-decoration-none text-muted"><ion-icon size="large" name="person-circle-outline"></ion-icon></a>
                 <a href="#" class="text-decoration-none text-muted p-4"><ion-icon size="large" name="gift-outline"></ion-icon></a>
                 <a href="#" class="text-decoration-none text-muted"><ion-icon size="large" name="cart-outline"></ion-icon></ion-icon></a>
