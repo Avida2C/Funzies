3025226dd0bb2d3fd4d6922189ceeebaeb8bae9f
Nadine <n4dinevid4l@hotmail.com>
2023-10-29 22:44:30 +0100

    Categories Updated in sliders.

--- include/navbar.php
+++ include/navbar.php
@@ -105,29 +105,26 @@
                     <!-- Slider Content -->
                     <div class="offcanvas-body m-2">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                                <a class="nav-link" href="#">Board games & Puzzles</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                                <a class="nav-link" href="#">Collectibles & Figurines</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                                <a class="nav-link" href="#">Funko</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Models</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Plushies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Posters</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Gifts</a>
                             </li>
                         </ul>
                     </div>
@@ -193,24 +190,27 @@
                             aria-labelledby="nav-categories-tab" tabindex="0">
                             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Features</a>
+                                    <a class="nav-link" href="#">Board games & Puzzles</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Pricing</a>
+                                    <a class="nav-link" href="#">Collectibles & Figurines</a>
                                 </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown"
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown link
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li>
-                                            <hr class="dropdown-divider">
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Funko</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Models</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Plushies</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Posters</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Gifts</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
