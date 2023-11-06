630955e19aab7aa8cc390b2d6b65c966511ff278
Nadine <n4dinevid4l@hotmail.com>
2023-11-06 10:19:38 +0100

    -Admin Page has been created -admin.css has been created

---
+++ admin/footer.php
@@ -0,0 +1,23 @@
+<footer>
+        <!-- Copyright-->
+        <div class="text-center bg-secondary text-secondary-emphasis row border-top pt-3">
+            <div class="col">
+                <p> Copyright &copy; <?php echo date("Y") ?> Funzies Collection. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+    <!-- Scripts -->
+    <!-- JQuery -->
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <!-- Javascript -->
+    <script src="js/javascript.js"></script>
+    <!-- Ionicons -->
+    <script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></script>
+
+</body>
+
+</html>
